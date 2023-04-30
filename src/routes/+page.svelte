04b759434a654cdfceb331d4377ca8a068cf4a79
ninetynin <wekandmusic@gmail.com>
2023-05-01 03:10:30 +0530

    p1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+    let { club } = data;
+    $: ({ club } = data);
+  </script>
+
+  <style>
+    div {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+article {
+  flex: 1 4 30%;
+  max-width: 30%;
+}
+  </style>
+<main class="container">
+  <ul>
+    {#each club as cl}
+        <!-- <a href="/clubs/{cl.club_name}">{cl.club_name}</a>
+        <br> -->
+        <div>
+            <article>
+                <a href="/clubs/{cl.club_name}">
+                    <h2>{cl.club_name}</h2>
+                </a>
+            </article>
+        </div>
+    {/each}
+  </ul>
+</main>
---
+++ src/routes/clubs/[club]/+layout.svelte
@@ -0,0 +1,18 @@
+<!-- <script>
+    // picocss
+
+</script> -->
+
+<style>
+    @import '@picocss/pico';
+</style>
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+    </ul>
+</nav>
+
+<main>
+    <slot/>
+</main>
