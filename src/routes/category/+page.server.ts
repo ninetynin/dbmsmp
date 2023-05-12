16b7b0ba84279723230690a625a3dd53e380e773
ninetynin <wekandmusic@gmail.com>
2023-05-12 08:10:02 +0530

    category done

--- src/routes/category/+page.server.ts
+++ src/routes/category/+page.server.ts
@@ -2,7 +2,9 @@ import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
   const { data:cdata } = await supabase.from("category").select();
+  const { data:c2data } = await supabase.from("club").select();
   return {
     category: cdata ?? [],
+    club: c2data ?? [],
   };
 }
